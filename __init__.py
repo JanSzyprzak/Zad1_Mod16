2e521132ef67d56e8bbc395a4644deb8bd09931a
Jan Szyprzak <jan.szyprzak@gmail.com>
2023-02-24 12:50:28 +0100

    Refactor entries function

--- __init__.py
+++ __init__.py
@@ -14,3 +14,9 @@ migrate = Migrate(app, db)
 
 from Blog import routes, models
 
+@app.shell_context_processor
+def make_shell_context():
+  return {
+      "db": db,
+      "Entry": models.Entry
+  }
